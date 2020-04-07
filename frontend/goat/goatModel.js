135ccc7c38cdee85d3c9532ffc33c88cd70bc842
Антон Матвиенко <a.matvienko@devlegal.ru>
2020-04-07 11:43:01 +0500

    Nothing

--- frontend/goat/goatModel.js
+++ frontend/goat/goatModel.js
@@ -1,16 +1,16 @@
 const RecvMsg = Object.freeze({
     YOUR_TEAM: 'yourTeam',
-    DEAL: 'deal', //*
+    DEAL: 'deal',
     HE_PUTS_CARD: 'hePutsCard', //*
     ASK_TRUMP: 'askTrump', //*
     HE_ASKS_TRUMP: 'heAsksTrump',
     TRUMP_IS: 'trumpIs',
-    YOUR_PARTIE_SCORE: 'yourPartieScore'
+    YOUR_PARTIE_SCORE: 'yourPartieScore' //*
 });
 
 const SendMsg = Object.freeze({
-    DETERM_TRUMP: 'determTrump',
-    PUT_CARD: 'putCard'
+    DETERM_TRUMP: 'determTrump', //*
+    PUT_CARD: 'putCard' //*
 });
 
 class CardTable extends GameTable {
