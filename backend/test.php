5c20549e4520124512d5bb7ff2a815548bc12c31
Антон Матвиенко <a.matvienko@devlegal.ru>
2020-04-09 13:54:56 +0500

    Move test ini settings to file from global config

--- backend/test.php
+++ backend/test.php
@@ -1,5 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+ini_set('xdebug.halt_level', E_WARNING | E_NOTICE | E_USER_WARNING | E_USER_NOTICE);
+ini_set('xdebug.max_nesting_level', 16384);
 
 use Games\Test\GoatServerTest;
 
